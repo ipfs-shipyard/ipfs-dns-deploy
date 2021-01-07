92c703567de937a7b23ca4a538b4cdb48aa82cca
thattommyhall <thattommyhall@gmail.com>
2021-01-07 15:42:54 +0100

    Add --wait to ipfs-cluster-ctl add

--- scripts/pin-to-cluster.sh
+++ scripts/pin-to-cluster.sh
@@ -58,7 +58,7 @@ root_cid=$(ipfs-cluster-ctl \
     --name "$PIN_NAME" \
     --recursive "$INPUT_DIR" ) || {
   # If it fails, show the ipfs-cluster-ctl command and the error message
-  echo "ipfs-cluster-ctl --host $HOST --basic-auth *** add --quieter --local --cid-version 1 --name '$PIN_NAME' --recursive $INPUT_DIR" 1>&2
+  echo "ipfs-cluster-ctl --host $HOST --basic-auth *** add --wait --quieter --local --cid-version 1 --name '$PIN_NAME' --recursive $INPUT_DIR" 1>&2
   echo "$root_cid" 1>&2
   echo "Failed to pin to cluster" 1>&2
   false
